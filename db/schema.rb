87476a018798203f2ce3a85feb1252f5299bc2c5
James P Bryan <bryanjp@bryanserver.com>
2013-11-05 16:39:51 -0500

    User and IncidentCase resources created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131105202427) do
+ActiveRecord::Schema.define(version: 20131105210916) do
+
+  create_table "cases", force: true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "incident_cases", force: true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
